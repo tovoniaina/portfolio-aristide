1f4ccf61210dc8c33d4c72d5ba78a6ffef6fe9ea
Aris <idealy371@gmail.com>
2023-07-15 07:03:28 +0300

    light/dark mode

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+  
+    themeToggle.addEventListener('change', function() {
+      if (this.checked) {
+        body.classList.add('light-theme');
+      } else {
+        body.classList.remove('light-theme');
+      }
     });
-    
-})();
+  
+    const controls = document.querySelectorAll(".control");
+    const sections = document.querySelectorAll(".section");
+  
+    controls.forEach(button => {
+      button.addEventListener("click", function() {
+        const activeButton = document.querySelector(".active-btn");
+        activeButton.classList.remove("active-btn");
+        this.classList.add("active-btn");
+  
+        const activeSection = document.querySelector(".active");
+        activeSection.classList.remove("active");
+  
+        const targetSection = document.getElementById(button.dataset.id);
+        targetSection.classList.add("active");
+      });
+    });
+  })();
+  
